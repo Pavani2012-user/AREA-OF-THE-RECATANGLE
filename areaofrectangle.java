4b766ba7d5573d1a3d9077cb27fe9378c3cdc25c
Pavani2012-user <pavanibattu006@gmail.com>
2025-01-25 12:39:04 +0530

    Add files via upload

---
+++ areaofrectangle.java
@@ -0,0 +1,10 @@
+public class areaofrectangle {
+        public static void main(String[] args) {
+            double length = 10.5; 
+            double width = 7.2;     
+            double area = length * width;
+            System.out.println("The area of the rectangle is: "+area);
+        }
+    }
+    
+
